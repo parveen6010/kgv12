6313731bdc170c81db1d9c4f90e9c20e27426c4d
parveen prajapati <77872164+parveen6010@users.noreply.github.com>
2024-01-29 16:57:10 +0530

    int

--- app.js
+++ app.js
@@ -55,24 +55,24 @@ app.post("/register", async (req, resp) => {
       // });
 
 
-      const transporter = nodemailer.createTransport({
-        host: process.env.host,
-        secure: false,
-        port: process.env.port,
-        service:process.env.service,
-        auth: {
-          user: process.env.user,
-          pass: process.env.pass,
-        },
-      });
-
       // const transporter = nodemailer.createTransport({
-      //       service: "gmail",
-      //       auth: {
-      //         user: "parveenprajapati9310@gmail.com", // Update with your Gmail address
-      //         pass: "davajvjvmpyfjlri", // Update with your Gmail password
-      //       },
-      //     });
+      //   host: process.env.host,
+      //   secure: false,
+      //   port: process.env.port,
+      //   service:process.env.service,
+      //   auth: {
+      //     user: process.env.user,
+      //     pass: process.env.pass,
+      //   },
+      // });
+
+      const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: "parveenprajapati9310@gmail.com", // Update with your Gmail address
+              pass: "davajvjvmpyfjlri", // Update with your Gmail password
+            },
+          });
         
       
     
@@ -106,83 +106,5 @@ app.post("/register", async (req, resp) => {
     resp.status(500).send(error.message);
   }
 });
-
-
-
-
-
-
-
-
-
-// import express from "express";
-// import { config } from "dotenv";
-// import { Contact } from "./models/contact.js";
-// import paymentRoutes from "./routes/paymentRoutes.js";
-// import cors from "cors";
-// import nodemailer from "nodemailer"; // Import nodemailer
-
-// config({ path: "./config/config.env" });
-// export const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use("/api", paymentRoutes);
-
-// app.get("/api/getkey", (req, res) =>
-//   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
-// );
-
-// app.post("/register", async (req, resp) => {
-//   try {
-//     // Save to the database
-//     let contact = new Contact(req.body);
-//     let result = await contact.save();
-
-//     // Send email notification
-//     sendEmailNotification(req.body);
-
-//     resp.send(result);
-//   } catch (error) {
-//     resp.status(500).send(error.message);
-//   }
-// });
-
-// // Function to send email notification
-
-//      function sendEmailNotification(formData) {
-//     const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: "parveenprajapati9310@gmail.com", // Update with your Gmail address
-//       pass: "davajvjvmpyfjlri", // Update with your Gmail password
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: "parveenprajapati9310@gmail.com",
-//     to: "parveenprajapati6010@gmail.com",
-//     subject: "New Registration",
-//     html: `<p>New registration details:</p>
-//            <p>Name: ${formData.name}</p>
-//            <p>Lastname: ${formData.lastname}</p>
-//            <p>Email: ${formData.email}</p>
-//            <p>Address: ${formData.address}</p>
-//            <p>Query: ${formData.query}</p>`,
-//   };
-
-
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log("Email error: " +error);
-//     } else {
-//       console.log("Email sent: " + info.response);
-//     }
-//   });
-// }
-
-
 
 
